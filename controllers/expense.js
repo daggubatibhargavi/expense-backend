9ebf4e99ac50cefb8656dd8a86eccfd56f97e3c1
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2025-05-24 16:17:56 +0530

    Update expense.js

--- controllers/expense.js
+++ controllers/expense.js
@@ -4,30 +4,60 @@ const ExpenseSchema = require("../models/ExpenseModel")
 exports.addExpense = async (req, res) => {
     const {title, amount, category, description, date}  = req.body
 
-    const income = ExpenseSchema({
-        title,
-        amount,
-        category,
-        description,
-        date
-    })
+//     const income = ExpenseSchema({
+//         title,
+//         amount,
+//         category,
+//         description,
+//         date
+//     })
 
-    try {
-        //validations
-        if(!title || !category || !description || !date){
-            return res.status(400).json({message: 'All fields are required!'})
-        }
-        if(amount <= 0 || !amount === 'number'){
-            return res.status(400).json({message: 'Amount must be a positive number!'})
-        }
-        await income.save()
-        res.status(200).json({message: 'Expense Added'})
-    } catch (error) {
-        res.status(500).json({message: 'Server Error'})
+//     try {
+//         //validations
+//         if(!title || !category || !description || !date){
+//             return res.status(400).json({message: 'All fields are required!'})
+//         }
+//         if(amount <= 0 || !amount === 'number'){
+//             return res.status(400).json({message: 'Amount must be a positive number!'})
+//         }
+//         await income.save()
+//         res.status(200).json({message: 'Expense Added'})
+//     } catch (error) {
+//         res.status(500).json({message: 'Server Error'})
+//     }
+
+//     console.log(income)
+// }
+const ExpenseSchema = require("../models/ExpenseModel");
+
+exports.addExpense = async (req, res) => {
+    const { title, amount, category, description, date } = req.body;
+
+    // Validation checks
+    if (!title || !category || !description || !date) {
+        return res.status(400).json({ message: 'All fields are required!' });
+    }
+    if (amount <= 0 || typeof amount !== 'number') {
+        return res.status(400).json({ message: 'Amount must be a positive number!' });
     }
 
-    console.log(income)
-}
+    try {
+        const expense = new ExpenseSchema({
+            title,
+            amount,
+            category,
+            description,
+            date,
+            type: 'expense'
+        });
+
+        await expense.save();
+
+        res.status(201).json({ message: 'Expense Added', data: expense });
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
 
 exports.getExpense = async (req, res) =>{
     try {
