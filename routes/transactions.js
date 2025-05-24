40349ceb55b6160e7f3abf83df680c4f3908f611
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2025-05-24 17:13:03 +0530

    Update transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,14 +1,15 @@
 const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = require('express').Router();
 
+router.post('/add-income', authMiddleware, addIncome);
+router.get('/get-incomes', authMiddleware, getIncomes);
+router.delete('/delete-income/:id', authMiddleware, deleteIncome);
 
-router.post('/add-income', addIncome)
-    .get('/get-incomes', getIncomes)
-    .delete('/delete-income/:id', deleteIncome)
-    .post('/add-expense', addExpense)
-    .get('/get-expenses', getExpense)
-    .delete('/delete-expense/:id', deleteExpense)
+router.post('/add-expense', authMiddleware, addExpense);
+router.get('/get-expenses', authMiddleware, getExpense);
+router.delete('/delete-expense/:id', authMiddleware, deleteExpense);
 
-module.exports = router
+module.exports = router;
